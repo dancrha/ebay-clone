a17401f10569bf39c5400df9fa47e11338767ac9
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-16 13:49:28 -0400

    webhook fixed

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,8 +1,8 @@
 import Stripe from "stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import prismadb from "@/lib/prismadb";
-import { stripe } from "@/lib/stripe";
+import prismadb from "@/libs/prismadb";
+import { stripe } from "@/libs/stripe";
 
 async function POST(req) {
   const body = await req.text();
