9f657cd8f91cabc879e3cca5bca1073d66a3a42b
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-16 12:54:30 -0400

    logo and favicon updated

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import UserProvider from "./context/user";
 import CartProvider from "./context/cart";
+import Head from "next/head";
 
 export const metadata = {
   title: "eBay Clone",
@@ -12,6 +13,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/images/favicon-16x16.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/images/favicon-32x32.png'
+        />
+        <link
+          rel='apple-touch-icon'
+          type='image/png'
+          sizes='180x180'
+          href='/images/apple-touch-icon.png'
+        />
+        <link rel='shortcut icon' href='/images/favicon.ico' />
+      </head>
+
       <body>
         <ToastContainer />
         <UserProvider>
