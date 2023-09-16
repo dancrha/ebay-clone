fd868c4998d654351d116e20b1b7f663adc1940f
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-16 13:33:41 -0400

    webhook

---
+++ app/api/webhook/route.js
@@ -0,0 +1,67 @@
+const Stripe = require("stripe");
+const { headers } = require("next/headers");
+const { NextResponse } = require("next/server");
+const prismadb = require("@/lib/prismadb");
+const { stripe } = require("@/lib/stripe");
+
+async function POST(req) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature");
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+
+  const session = event.data.object;
+
+  if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription
+    );
+
+    if (!session?.metadata?.userId) {
+      return new NextResponse("User id is required", { status: 400 });
+    }
+
+    await prismadb.userSubscription.create({
+      data: {
+        userId: session?.metadata?.userId,
+        stripeSubscriptionId: subscription.id,
+        stripeCustomerId: subscription.customer,
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+  if (event.type === "invoice.payment_succeeded") {
+    const subscription = await stripe.subscriptions.retrieve(
+      session.subscription
+    );
+    await prismadb.userSubscription.update({
+      where: {
+        stripeSubscriptionId: subscription.id,
+      },
+      data: {
+        stripePriceId: subscription.items.data[0].price.id,
+        stripeCurrentPeriodEnd: new Date(
+          subscription.current_period_end * 1000
+        ),
+      },
+    });
+  }
+  return new NextResponse(null, { status: 200 });
+}
+
+module.exports = {
+  POST,
+};
