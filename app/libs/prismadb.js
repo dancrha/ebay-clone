07107b678d967a78a5adf91bd8ff6ad6c6871243
Dan Crha <44410856+danielcrha@users.noreply.github.com>
2023-09-16 13:58:34 -0400

    reset

--- app/libs/prismadb.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-globalThis.prisma = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production")
-  globalThis.prisma = globalThis.prisma;
-
-module.exports = globalThis.prisma;
